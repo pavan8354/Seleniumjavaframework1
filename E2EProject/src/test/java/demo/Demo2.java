30a4bbd460c83cca59a578e364bc07e1971a5ed5
pavan <pavansdmitmech@gmail.com>
2020-06-02 21:38:07 +0530

    This is new message that i added for testing

---
+++ E2EProject/src/test/java/demo/Demo2.java
@@ -0,0 +1,18 @@
+package demo;
+
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Demo2 {
+
+	
+		
+		
+		 
+		
+		
+	}
+
